154cb70ca84147ac3e0e8b543920b1f62e436acb
haruka9147 <haruphonexxx@gmail.com>
2018-04-22 11:48:53 -0700

    first commit

---
+++ Assignment7/Assignment7/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment7
+//
+//  Created by 北田晴佳 on 2018/04/22.
+//  Copyright © 2018 com.Haruka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
